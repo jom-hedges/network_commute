0f8f88c6aecaf7592e7cbb6e66f8afcc70989144
jom-hedges <jom_hedges@proton.me>
2024-06-20 11:10:39 +0800

    init commit

---
+++ lib/network_commute.ex
@@ -0,0 +1,18 @@
+defmodule NetworkCommute do
+  @moduledoc """
+  Documentation for `NetworkCommute`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NetworkCommute.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/network_commute_test.exs
@@ -0,0 +1,8 @@
+defmodule NetworkCommuteTest do
+  use ExUnit.Case
+  doctest NetworkCommute
+
+  test "greets the world" do
+    assert NetworkCommute.hello() == :world
+  end
+end
